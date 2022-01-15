e809c01719c67e23e756774201d7c51aa951c91d
Evgeniy Terekhin <e_terekhin@pm.me>
2022-01-15 10:10:36 +0200

    add build env

---
+++ build/x86_64-unknown-linux-gnu.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export CC_x86_64_unknown_linux_gnu=x86_64-unknown-linux-gnu-gcc
+export CXX_x86_64_unknown_linux_gnu=x86_64-unknown-linux-gnu-g++
+export AR_x86_64_unknown_linux_gnu=x86_64-unknown-linux-gnu-ar
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=x86_64-unknown-linux-gnu-gcc
